2bbaefad83245f8ed194aeafaee307027ad2f26d
Dmitriy Marmyshev <marmyshev@gmail.com>
2013-04-11 12:26:41 +0400

    added names

--- lang/ipodcast.en.lang.php
+++ lang/ipodcast.en.lang.php
@@ -3,7 +3,7 @@
  * English language file for ipodcast
  *
  * @package Cotonti
- * @version 0.0.1
+ * @version 1.0.1
  * @author Dmitriy Marmyshev
  * @copyright (c) 2011 Dmitriy Marmyshev
  * @license BSD License
@@ -11,25 +11,28 @@
  
  defined('COT_CODE') or die('Wrong URL.');
  
-$L['cfg_ipodcast_channeltitle']= array('Title of podcast');
-$L['cfg_ipodcast_channellink']= array('Link of podcast');
-$L['cfg_ipodcast_channellang']= array('Language of podcast');
-$L['cfg_ipodcast_channelcopy']= array('Copyright of podcast');
-$L['cfg_ipodcast_channelsubtitle']= array('Subtitle of podcast');
-$L['cfg_ipodcast_channelauthor']= array('Author of podcast');
-$L['cfg_ipodcast_channelsummary']= array('Summary of podcast');
-$L['cfg_ipodcast_channeldescription']= array('Description of podcast');
-$L['cfg_ipodcast_channelownername']= array('Owner Name');
-$L['cfg_ipodcast_channelowneremail']= array('Owner email');
-$L['cfg_ipodcast_channelimage']= array('Image link of podcast');
-$L['cfg_ipodcast_channelcategories']= array('Categories of podcast (splited by comma)');
-$L['cfg_ipodcast_maxitems']= array('Max items in podcast');
-$L['cfg_ipodcast_catalog']= array('Catalog of site for podcast');
-$L['cfg_ipodcast_descusepagetext']= array('Use page text for description');
-$L['cfg_ipodcast_showchannelimage']= array('Show channel image for item if itself image empty');
-$L['cfg_ipodcast_channelexplicit']= array('Podcast contains explicit material');
-$L['cfg_ipodcast_useredirecturl']= array('Use redirect URL for podcast');
-$L['cfg_ipodcast_redirecturl']= array('Redirect URL for podcast (for external feed system)');
+$L['cfg_ipodcast_channeltitle']= array('Title of podcast', ' ');
+$L['cfg_ipodcast_channellink']= array('Link of podcast', ' ');
+$L['cfg_ipodcast_channellang']= array('Language of podcast', ' ');
+$L['cfg_ipodcast_channelcopy']= array('Copyright of podcast', ' ');
+$L['cfg_ipodcast_channelsubtitle']= array('Subtitle of podcast', ' ');
+$L['cfg_ipodcast_channelauthor']= array('Author of podcast', ' ');
+$L['cfg_ipodcast_channelsummary']= array('Summary of podcast', ' ');
+$L['cfg_ipodcast_channeldescription']= array('Description of podcast', ' ');
+$L['cfg_ipodcast_channelownername']= array('Owner Name', ' ');
+$L['cfg_ipodcast_channelowneremail']= array('Owner email', ' ');
+$L['cfg_ipodcast_channelimage']= array('Image link of podcast', ' ');
+$L['cfg_ipodcast_channelcategories']= array('Categories of podcast (splited by comma)', ' ');
+$L['cfg_ipodcast_maxitems']= array('Max items in podcast', ' ');
+$L['cfg_ipodcast_catalog']= array('Catalog of site for podcast', ' ');
+$L['cfg_ipodcast_descusepagetext']= array('Use page text for description', ' ');
+$L['cfg_ipodcast_showchannelimage']= array('Show channel image for item if itself image empty', ' ');
+$L['cfg_ipodcast_channelexplicit']= array('Podcast contains explicit material', ' ');
+$L['cfg_ipodcast_useredirecturl']= array('Use redirect URL for podcast', ' ');
+$L['cfg_ipodcast_redirecturl']= array('Redirect URL for podcast (for external feed system)', ' ');
+$L['cfg_ipodcast_timetolive']= array('Refresh RSS cache every N seconds', ' ');
+$L['cfg_ipodcast_charset']= array('RSS charset', ' ');
+$L['cfg_ipodcast_pagemaxsymbols']= array('Pages. Cut element description longer than N symbols', 'Disabled by default');
 
 
 ?>
